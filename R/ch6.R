0e9170a7716ef21105aa30917be5d2a20af4983f
Schuster, Zachary <u777304@emn.com>
2019-03-22 08:24:15 -0400

    finish up ch6

--- R/ch6.R
+++ R/ch6.R
@@ -105,3 +105,75 @@ f2 <- function(x = z) {
 # This function will return 100 as x evaluates to z which is defined inside the
 # function body
 f2()
+
+
+
+# 6.5.3 -------------------------------------------------------------------
+
+# 1
+base_funs = builtins()
+contain_left_arrow = grepl(".*(<-).*", x = base_funs)
+
+# list of all replacement functions
+(replacement_funs = base_funs[contain_left_arrow])
+
+# which ones are primitive?
+
+primitive_ind = unlist(vapply(replacement_funs, function(x) {
+  is.primitive(match.fun(x))
+}, FUN.VALUE = logical(1)))
+
+replacement_funs[primitive_ind]
+
+# 2 
+# starts with %, anything other than a % symbol (escape special characters),
+# ends with %
+
+# 3
+`%xor%` = function(x, y){
+  (x |y) & !(x & y)
+}
+
+xor(TRUE, TRUE); TRUE %xor% TRUE
+xor(TRUE, FALSE); TRUE %xor% FALSE
+xor(FALSE, FALSE); FALSE %xor% FALSE
+
+# 4
+`%intersect%` = function (x, y) {
+  y <- as.vector(y)
+  unique(y[match(as.vector(x), y, 0L)])
+}
+
+intersect(15:25, 20:30)
+15:25 %intersect% 20:30
+
+# 5
+`rand_mod<-` = function(x, value = 100){
+  len = length(x)
+  pos = round(runif(1, 1, len))
+  x[pos] = value
+  return(x)
+}
+
+vec = 1:10
+rand_mod(vec) = 15
+vec
+
+
+# 6.6.2 -------------------------------------------------------------------
+
+# 1
+# prefer chdir because it has error checking for if current wd can't be found
+
+# 2
+# remove.packages()
+
+# get the old option using getOption
+
+# 3
+# let john do this
+
+# 4
+capture.output
+# Hadley removed much of the error handlind and extra options in on.exit
+# Different write modes were also removed
